64bc7e22c6d5d5572782505e878567f12869b94f
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-04-02 14:58:48 +0300

    BAP-3719: Fusion Charts support for charts - fix b2b controller

--- src/OroCRMPro/Bundle/FusionCharts/Controller/Dashboard/SalesFlowB2BController.php
+++ src/OroCRMPro/Bundle/FusionCharts/Controller/Dashboard/SalesFlowB2BController.php
@@ -11,6 +11,32 @@ class SalesFlowB2BController extends BaseDashboardController
 {
     /**
      * @Route(
+     *      "/opportunities_by_lead_source/chart/{widget}",
+     *      name="orocrmpro_fusioncharts_dashboard_opportunities_by_lead_source_chart",
+     *      requirements={"widget"="[\w-]+"}
+     * )
+     * @Template("OroCRMProFusionChartsBundle:Dashboard:pieChart.html.twig")
+     */
+    public function opportunitiesByLeadSourceAction($widget)
+    {
+        return parent::opportunitiesByLeadSourceAction($widget);
+    }
+
+    /**
+     * @Route(
+     *      "/opportunity_state/chart/{widget}",
+     *      name="orocrmpro_fusioncharts_dashboard_opportunity_by_state_chart",
+     *      requirements={"widget"="[\w-]+"}
+     * )
+     * @Template("OroCRMProFusionChartsBundle:Dashboard:opportunityByStatus.html.twig")
+     */
+    public function opportunityByStatusAction($widget)
+    {
+        return parent::opportunityByStatusAction($widget);
+    }
+
+    /**
+     * @Route(
      *      "/sales_flow_b2b/chart/{widget}",
      *      name="orocrmpro_fusioncharts_dashboard_sales_flow_b2b_chart",
      *      requirements={"widget"="[\w_-]+"}
