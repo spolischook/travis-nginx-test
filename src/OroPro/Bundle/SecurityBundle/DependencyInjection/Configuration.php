e16ff5114494ad5527e3cc9150c314d88674eb1d
Dmitry Khrysev <dkhrysev@magecore.com>
2014-04-16 12:45:07 +0300

    CRM-1058: License authorization and validation of Enterprise version

---
+++ src/OroPro/Bundle/SecurityBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OroPro\Bundle\SecurityBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_pro_security');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
