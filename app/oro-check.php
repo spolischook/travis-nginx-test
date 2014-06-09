63f13b4ce2d3130420af509fb483f96fe0223f99
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-06-09 17:52:15 +0300

    OC-251: Check PhpExecutableFinder Version - check via cli - remove listener

--- app/oro-check.php
+++ app/oro-check.php
@@ -1,24 +1,7 @@
 <?php
 
 require_once dirname(__FILE__) . '/OroRequirements.php';
-$classMap = require_once dirname(__FILE__) . '/../vendor/composer/autoload_classmap.php';
-
-$requiredClasses = array(
-    'Symfony\Component\Process\ProcessUtils',
-    'Symfony\Component\Process\Process',
-    'Symfony\Component\Process\ProcessPipes',
-    'Symfony\Component\Process\ProcessBuilder',
-    'Symfony\Component\Intl\Intl',
-    'Symfony\Component\Process\PhpExecutableFinder',
-
-    'Oro\Bundle\InstallerBundle\Process\PhpExecutableFinder',
-);
-
-foreach ($requiredClasses as $requiredClass) {
-    if (isset($classMap[$requiredClass])) {
-        require_once $classMap[$requiredClass];
-    }
-}
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 $oroRequirements = new OroRequirements();
 
