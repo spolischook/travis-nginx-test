5289c24969e6d3a1411e674c858221c5823b7790
Alexander Nezdoiminoga <alex@magecore.com>
2014-07-30 17:31:20 +0300

    OEE-49: CS fixes

--- src/OroPro/Bundle/OrganizationBundle/OroProOrganizationBundle.php
+++ src/OroPro/Bundle/OrganizationBundle/OroProOrganizationBundle.php
@@ -2,20 +2,8 @@
 
 namespace OroPro\Bundle\OrganizationBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-//use OroPro\Bundle\OrganizationBundle\DependencyInjection\Compiler\OwnerDeletionManagerPass;
 
 class OroProOrganizationBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        //$container->addCompilerPass(new OwnerDeletionManagerPass());
-    }
 }
