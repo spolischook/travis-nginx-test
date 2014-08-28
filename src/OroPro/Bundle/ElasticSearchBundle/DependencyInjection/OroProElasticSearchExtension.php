cd70274c71a3fa46463bdda9da1cbfbd14db5ae7
Viktor Linkin <vlinkin@magecore.com>
2014-08-28 16:43:01 +0300

    OEE-225: Add appropriate configuration for ElasticSearch engine - small fixes

--- src/OroPro/Bundle/ElasticSearchBundle/DependencyInjection/OroProElasticSearchExtension.php
+++ src/OroPro/Bundle/ElasticSearchBundle/DependencyInjection/OroProElasticSearchExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class OroElasticSearchExtension extends Extension
+class OroProElasticSearchExtension extends Extension
 {
     /**
      * {@inheritDoc}
