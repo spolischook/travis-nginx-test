712959390d9807a84e56b46c859336c8b16e97f1
Yevhen Shyskin <shishkin.yevgeniy@gmail.com>
2014-09-02 18:56:23 +0300

    OEE-228: Implement methods save and delete in ElasticSearchEngine  - added raw methods

--- src/OroPro/Bundle/ElasticSearchBundle/Engine/ElasticSearch.php
+++ src/OroPro/Bundle/ElasticSearchBundle/Engine/ElasticSearch.php
@@ -47,23 +47,74 @@ class ElasticSearch extends AbstractEngine
     /**
      * {@inheritdoc}
      */
+    public function save($entity, $realTime = true)
+    {
+        return $this->processEntities($entity, $realTime, true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($entity, $realTime = true)
+    {
+        return $this->processEntities($entity, $realTime, false);
+    }
+
+    /**
+     * @param object|array $entity
+     * @param bool $realTime
+     * @param bool $isSave
+     * @return bool
+     */
+    protected function processEntities($entity, $realTime, $isSave)
+    {
+        $entities = $this->getEntitiesArray($entity);
+        if (!$entities) {
+            return false;
+        }
+
+        if (!$realTime) {
+            $this->scheduleIndexation($entities);
+            return true;
+        }
+
+        $body = array();
+
+        foreach ($entities as $entity) {
+            $type = $this->getEntityAlias($this->doctrineHelper->getEntityClass($entity));
+            $id   = (string)$this->doctrineHelper->getSingleEntityIdentifier($entity);
+            if (!$type || !$id) {
+                continue;
+            }
+
+            // need to recreate index to avoid saving of not used fields
+            $indexIdentifier = array('_type' => $type, '_id' => $id);
+            $body[] = array('delete' => $indexIdentifier);
+
+            if ($isSave) {
+                $indexData = $this->getIndexData($entity);
+                if (!$indexData) {
+                    continue;
+                }
+
+                $body[] = array('create' => $indexIdentifier);
+                $body[] = $indexData;
+            }
+        }
+
+        if (!$body) {
+            return false;
+        }
+
+        $response = $this->getClient()->bulk(array('index' => $this->indexAgent->getIndexName(), 'body' => $body));
+
+        return empty($response['errors']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function reindex($class = null)
-    {
-        return 0;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($entity, $realtime = true)
-    {
-        return 0;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function save($entity, $realtime = true)
     {
         return 0;
     }
@@ -88,5 +139,30 @@ class ElasticSearch extends AbstractEngine
         }
 
         return $this->client;
+    }
+
+    /**
+     * @param string $class
+     * @return string|null
+     */
+    protected function getEntityAlias($class)
+    {
+        $aliases = $this->mapper->getEntitiesListAliases();
+
+        return !empty($aliases[$class]) ? $aliases[$class] : null;
+    }
+
+    /**
+     * @param object $entity
+     * @return array
+     */
+    protected function getIndexData($entity)
+    {
+        $indexData = array();
+        foreach ($this->mapper->mapObject($entity) as $fields) {
+            $indexData = array_merge($indexData, $fields);
+        }
+
+        return $indexData;
     }
 }
