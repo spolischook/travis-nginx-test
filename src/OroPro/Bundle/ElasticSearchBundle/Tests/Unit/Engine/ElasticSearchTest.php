99c81bd5ed049ee9e42b731422b6250113b037e7
Yevhen Shyshkin <shishkin.yevgeniy@gmail.com>
2014-09-04 13:26:23 +0300

    OEE-228: Implement methods save and delete in ElasticSearchEngine  - added tests for save and delete methods

--- src/OroPro/Bundle/ElasticSearchBundle/Tests/Unit/Engine/ElasticSearchTest.php
+++ src/OroPro/Bundle/ElasticSearchBundle/Tests/Unit/Engine/ElasticSearchTest.php
@@ -11,6 +11,8 @@ use OroPro\Bundle\ElasticSearchBundle\Tests\Unit\Stub\TestEntity;
 class ElasticSearchTest extends \PHPUnit_Framework_TestCase
 {
     const TEST_CLASS = 'Stub\TestEntity';
+    const TEST_ALIAS = 'test_entity';
+    const TEST_INDEX = 'test_index';
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
@@ -69,6 +71,28 @@ class ElasticSearchTest extends \PHPUnit_Framework_TestCase
                 )
             );
 
+        $this->mapper->expects($this->any())->method('mapObject')
+            ->with($this->isInstanceOf('OroPro\Bundle\ElasticSearchBundle\Tests\Unit\Stub\TestEntity'))
+            ->will(
+                $this->returnCallback(
+                    function (TestEntity $entity) {
+                        $map = array('text' => array());
+                        if ($entity->firstName) {
+                            $map['text']['firstName'] = $entity->firstName;
+                        }
+                        if ($entity->lastName) {
+                            $map['text']['lastName'] = $entity->lastName;
+                        }
+                        return $map;
+                    }
+                )
+            );
+        $this->mapper->expects($this->any())->method('getEntitiesListAliases')
+            ->will($this->returnValue(array(self::TEST_CLASS => self::TEST_ALIAS)));
+
+        $this->indexAgent->expects($this->any())->method('getIndexName')
+            ->will($this->returnValue(self::TEST_INDEX));
+
         $this->engine = new ElasticSearch(
             $this->registry,
             $this->eventDispatcher,
@@ -79,34 +103,41 @@ class ElasticSearchTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @param object|array $entity
+     * @param array $jobArguments
+     * @param bool $result
      * @param bool $isSave
      * @dataProvider queuedOperationDataProvider
      */
-    public function testQueuedOperation($isSave)
+    public function testQueuedOperation($entity, array $jobArguments, $result, $isSave)
     {
-        $entities = array(new TestEntity(1), new TestEntity(2));
-
         $entityManager = $this->getMockBuilder('Doctrine\ORM\EntityManager')
             ->disableOriginalConstructor()
             ->getMock();
-        $entityManager->expects($this->once())->method('persist')
-            ->with($this->isInstanceOf('JMS\JobQueueBundle\Entity\Job'))
-            ->will(
-                $this->returnCallback(
-                    function (Job $job) {
-                        $this->assertEquals($job->getCommand(), IndexCommand::NAME);
-                        $this->assertEquals(array(self::TEST_CLASS, 1, 2), $job->getArgs());
-                    }
-                )
-            );
+        if ($entity) {
+            $entityManager->expects($this->once())->method('persist')
+                ->with($this->isInstanceOf('JMS\JobQueueBundle\Entity\Job'))
+                ->will(
+                    $this->returnCallback(
+                        function (Job $job) use ($jobArguments) {
+                            $this->assertEquals($job->getCommand(), IndexCommand::NAME);
+                            $this->assertEquals($jobArguments, $job->getArgs());
+                        }
+                    )
+                );
+            $entityManager->expects($this->once())->method('flush');
+        } else {
+            $entityManager->expects($this->never())->method('persist');
+            $entityManager->expects($this->never())->method('flush');
+        }
 
         $this->registry->expects($this->any())->method('getManagerForClass')->with('JMSJobQueueBundle:Job')
             ->will($this->returnValue($entityManager));
 
         if ($isSave) {
-            $this->engine->save($entities, false);
+            $this->assertEquals($result, $this->engine->save($entity, false));
         } else {
-            $this->engine->delete($entities, false);
+            $this->assertEquals($result, $this->engine->delete($entity, false));
         }
     }
 
@@ -116,8 +147,128 @@ class ElasticSearchTest extends \PHPUnit_Framework_TestCase
     public function queuedOperationDataProvider()
     {
         return array(
-            'save' => array(true),
-            'delete' => array(false),
+            'save with entities' => array(
+                'entity' => array(new TestEntity(1), new TestEntity(2)),
+                'jobArguments' => array(self::TEST_CLASS, 1, 2),
+                'result' => true,
+                'isSave' => true
+            ),
+            'save without entities' => array(
+                'entity' => array(),
+                'jobArguments' => array(),
+                'result' => false,
+                'isSave' => true
+            ),
+            'delete with entities' => array(
+                'entity' => new TestEntity(1),
+                'jobArguments' => array(self::TEST_CLASS, 1),
+                'result' => true,
+                'isSave' => false
+            ),
+            'delete without entities' => array(
+                'entity' => null,
+                'jobArguments' => array(),
+                'result' => false,
+                'isSave' => false
+            ),
+        );
+    }
+
+    /**
+     * @param object|array $entity
+     * @param array $body
+     * @param array $response
+     * @param bool $result
+     * @param bool $isSave
+     * @dataProvider realTimeOperationDataProvider
+     */
+    public function testRealTimeOperation($entity, array $body, array $response, $result, $isSave)
+    {
+        $client = $this->getMockBuilder('Elasticsearch\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+        if ($body) {
+            $client->expects($this->once())->method('bulk')->with(array('index' => self::TEST_INDEX, 'body' => $body))
+                ->will($this->returnValue($response));
+        } else {
+            $client->expects($this->never())->method('bulk');
+        }
+
+        $this->indexAgent->expects($this->any())->method('initializeClient')
+            ->will($this->returnValue($client));
+
+        if ($isSave) {
+            $this->assertEquals($result, $this->engine->save($entity));
+        } else {
+            $this->assertEquals($result, $this->engine->delete($entity));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function realTimeOperationDataProvider()
+    {
+        return array(
+            'save successful' => array(
+                'entity' => array(new TestEntity(1, 'firstName1', 'lastName1'), new TestEntity(2, 'firstName2')),
+                'body' => array(
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                    array('create' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                    array('firstName' => 'firstName1', 'lastName' => 'lastName1'),
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 2)),
+                    array('create' => array('_type' => self::TEST_ALIAS, '_id' => 2)),
+                    array('firstName' => 'firstName2'),
+                ),
+                'response' => array('errors' => false),
+                'result' => true,
+                'isSave' => true
+            ),
+            'save not successful' => array(
+                'entity' => array(new TestEntity(1, 'firstName1', 'lastName1'), new TestEntity(2)),
+                'body' => array(
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                    array('create' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                    array('firstName' => 'firstName1', 'lastName' => 'lastName1'),
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 2)),
+                ),
+                'response' => array('errors' => true),
+                'result' => false,
+                'isSave' => true
+            ),
+            'save without body' => array(
+                'entity' => array(new TestEntity()),
+                'body' => array(),
+                'response' => array(),
+                'result' => false,
+                'isSave' => true
+            ),
+            'delete successful' => array(
+                'entity' => array(new TestEntity(1, 'firstName1', 'lastName1'), new TestEntity(2, 'firstName2')),
+                'body' => array(
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 2)),
+                ),
+                'response' => array('errors' => false),
+                'result' => true,
+                'isSave' => false
+            ),
+            'delete not successful' => array(
+                'entity' => new TestEntity(1, 'firstName1', 'lastName1'),
+                'body' => array(
+                    array('delete' => array('_type' => self::TEST_ALIAS, '_id' => 1)),
+                ),
+                'response' => array('errors' => true),
+                'result' => false,
+                'isSave' => false
+            ),
+            'delete without body' => array(
+                'entity' => array(new TestEntity()),
+                'body' => array(),
+                'response' => array(),
+                'result' => false,
+                'isSave' => false
+            ),
         );
     }
 }
