a0bc9c9f7be6acb8f4b71858d2834cf195fa1f85
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-09-26 18:53:07 +0300

    BAP-5399: Organizations refactoring

---
+++ src/OroPro/Bundle/OrganizationBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OroPro\Bundle\OrganizationBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OverrideServiceCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $serviceId = 'oro_organization.form.type.organizations_select';
+        if ($container->hasDefinition($serviceId)) {
+            $definition = $container->getDefinition($serviceId);
+            $definition->setClass('OroPro\Bundle\OrganizationBundle\Form\Type\OrganizationsProSelectType');
+        }
+    }
+}
