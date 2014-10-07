f60ad9ff798125caf6bffa74d7110624f6e19a76
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-10-07 13:19:36 +0300

    OEE-346: fixes

--- src/OroPro/Bundle/OrganizationBundle/Resources/public/js/init-applicable-organization-selector.js
+++ src/OroPro/Bundle/OrganizationBundle/Resources/public/js/init-applicable-organization-selector.js
@@ -1,28 +1,28 @@
-/* global require */
-require(
-    ['jquery'],
-    function ($) {
-        'use strict';
-        $(function() {
-            var all_selector = $('input.all-selector:checkbox');
-            var selective_selectors = $('div.selective-selector input:checkbox');
+/*jslint browser:true, nomen:true*/
+/*global define*/
+require([
+    'jquery'
+], function ($) {
+    'use strict';
 
-            if ($(all_selector).prop('checked') === true) {
-                $(selective_selectors).each(function(index, el){
-                    $(el).prop('disabled', true);
-                });
-            }
-            $(document).on('change', 'input.all-selector:checkbox', function () {
-                if($(this).prop('checked') === true) {
-                    $(selective_selectors).each(function(index, el){
-                        $(el).prop('disabled', true);
-                    });
-                } else {
-                    $(selective_selectors).each(function(index, el){
-                        $(el).removeAttr('disabled');
-                    });
-                }
-            });
+    var all_selector = $('input.all-selector:checkbox');
+    var selective_selectors = $('div.selective-selector input:checkbox');
+
+    if ($(all_selector).prop('checked') === true) {
+        $(selective_selectors).each(function(index, el){
+            $(el).prop('disabled', true);
         });
     }
-);
+
+    $(document).on('change', 'input.all-selector:checkbox', function () {
+        if($(this).prop('checked') === true) {
+            $(selective_selectors).each(function(index, el){
+                $(el).prop('disabled', true);
+            });
+        } else {
+            $(selective_selectors).each(function(index, el){
+                $(el).removeAttr('disabled');
+            });
+        }
+    });
+});
