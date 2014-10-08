ba7d44cd2617444f74859e21c8d7a9e6862da68c
Alexander Nezdoiminoga <anezdoiminoga@magecore.com>
2014-10-08 16:05:24 +0300

    OEE-347

--- src/OroPro/Bundle/OrganizationBundle/Twig/EntityDataExtension.php
+++ src/OroPro/Bundle/OrganizationBundle/Twig/EntityDataExtension.php
@@ -3,21 +3,32 @@
 namespace OroPro\Bundle\OrganizationBundle\Twig;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManager;
+
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\TranslationBundle\Translation\Translator;
 
 class EntityDataExtension extends \Twig_Extension
 {
-    const NAME = 'oro_entity_data';
+    const NAME = 'oropro_organization';
 
     /** @var  Registry */
     protected $doctrine;
 
+    /** @var Translator */
+    protected $translator;
+
     /**
-     * @param Registry $doctrine
+     * @param Registry   $doctrine
+     * @param Translator $translator
      */
-    public function __construct(Registry $doctrine)
-    {
-        $this->doctrine = $doctrine;
+    public function __construct(
+        Registry $doctrine,
+        Translator $translator
+    ) {
+        $this->doctrine   = $doctrine;
+        $this->translator = $translator;
     }
 
     /**
@@ -26,28 +37,47 @@ class EntityDataExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('oro_entity_data', array($this, 'getEntityData')),
+            new \Twig_SimpleFunction('oropro_applicable_organizations', array($this, 'getApplicable')),
         );
     }
 
     /**
-     * @param string $entityClass
-     * @param array  $ids
+     * Used to render configuration value "applicable" from "organization" configuration scope.
+     * Due to restriction that configuration property which you want to show on grid should be indexed,
+     * and complicated entity config form type "oro_type_choice_organization_type" that stores multiple values.
      *
-     * @return array
+     * @param string $value
+     *
+     * @return string
      */
-    public function getEntityData($entityClass, $ids = [])
+    public function getApplicable($value)
     {
-        /**
-         * TODO: retrive organization names for given organization ids
-         */
+        $result = $this->translator->trans('oropro.organization.datagrid.applicable_none');
 
-        /** @var EntityManager $em */
-        $em = $this->doctrine->getManager();
-        $data = [];
+        /** @var \stdClass $data */
+        $data = json_decode($value);
+        if (is_object($data)) {
+            if ($data->all === true) {
+                $result = $this->translator->trans('oropro.organization.datagrid.applicable_all');
+            } elseif (!empty($data->selective)) {
+                /** @var EntityManager $em */
+                $em = $this->doctrine->getManager();
 
-        return $data;
+                $expr     = Criteria::expr();
+                $criteria = Criteria::create();
+                $criteria->where($expr->in('id', $data->selective));
 
+                /** @var Organization[] $organizations */
+                $organizations   = $em->getRepository('OroOrganizationBundle:Organization')->matching($criteria);
+                foreach ($organizations as $organization) {
+                    $result[] = $organization->getName();
+                }
+
+                $result = implode(', ', $result);
+            }
+        }
+
+        return $result;
     }
 
     /**
