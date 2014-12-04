f393b64f4f2c69157ed8706b8bae92cf30dc0699
Alexander Nezdoiminoga <alex@magecore.com>
2014-12-04 15:39:16 +0200

    OEE-406: view extension for custom fields in Global view mode

--- src/OroPro/Bundle/OrganizationBundle/Twig/DynamicFieldsExtension.php
+++ src/OroPro/Bundle/OrganizationBundle/Twig/DynamicFieldsExtension.php
@@ -6,16 +6,25 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 use Oro\Bundle\EntityConfigBundle\Config\ConfigInterface;
 use Oro\Bundle\EntityConfigBundle\Config\ConfigManager;
-use Oro\Bundle\EntityExtendBundle\Extend\FieldTypeHelper;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
+
 use Oro\Bundle\SecurityBundle\SecurityFacade;
 
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
+
+use Oro\Bundle\EntityExtendBundle\Extend\FieldTypeHelper;
 use Oro\Bundle\EntityExtendBundle\Twig\DynamicFieldsExtension as BaseDynamicFieldsExtension;
 
 class DynamicFieldsExtension extends BaseDynamicFieldsExtension
 {
     /** @var SecurityFacade */
     protected $securityFacade;
+
+    /** @var ConfigProvider */
     protected $organizationProvider;
+
+    /** @var Organization|null */
+    protected $entityOrganization = null;
 
     /**
      * @param ConfigManager            $configManager
@@ -28,11 +37,26 @@ class DynamicFieldsExtension extends BaseDynamicFieldsExtension
         FieldTypeHelper $fieldTypeHelper,
         EventDispatcherInterface $dispatcher,
         SecurityFacade $securityFacade
-    ) {
+    )
+    {
         parent::__construct($configManager, $fieldTypeHelper, $dispatcher);
 
-        $this->securityFacade = $securityFacade;
+        $this->securityFacade       = $securityFacade;
         $this->organizationProvider = $configManager->getProvider('organization');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFields($entity, $entityClass = null)
+    {
+        if (method_exists($entity, 'getOrganization')) {
+            $this->entityOrganization = $entity->getOrganization();
+        } else {
+            $this->entityOrganization = $this->securityFacade->getOrganization();
+        }
+
+        return parent::getFields($entity, $entityClass);
     }
 
     /**
@@ -42,15 +66,16 @@ class DynamicFieldsExtension extends BaseDynamicFieldsExtension
     {
         if (parent::filterFields($config)) {
             $organizationConfig = $this->organizationProvider->getConfigById($config->getId());
+            $organizationId     = $this->securityFacade->getOrganization()->getIsGlobal()
+                ? $this->entityOrganization->getId()
+                : $this->securityFacade->getOrganizationId();
 
             // skip field if it's not configured for current organization
             $applicable = $organizationConfig->get('applicable', false, false);
+
             return
                 $applicable
-                && (
-                    $applicable['all']
-                    || in_array($this->securityFacade->getOrganizationId(), $applicable['selective'])
-                );
+                && ($applicable['all'] || in_array($organizationId, $applicable['selective']));
         }
 
         return false;
