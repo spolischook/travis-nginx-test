dfd833b6beaacf4418f3bf491062911b7e788188
Alexander Nezdoiminoga <alex@magecore.com>
2014-12-17 14:41:23 +0200

    OEE-423: code cleanup

--- src/OroPro/Bundle/OrganizationBundle/Form/EventListener/OrganizationFieldListener.php
+++ src/OroPro/Bundle/OrganizationBundle/Form/EventListener/OrganizationFieldListener.php
@@ -37,33 +37,21 @@ class OrganizationFieldListener
             $environment = $event->getTwigEnvironment();
             $data        = $event->getFormData();
             $form        = $event->getForm();
-            $label       = false;
-
-            /*$entityProvider = $this->configManager->getProvider('entity');
-            if (is_object($form->vars['value'])) {
-                $className = ClassUtils::getClass($form->vars['value']);
-                if (class_exists($className)
-                    && $entityProvider->hasConfig($className, 'owner')
-                ) {
-                    $config = $entityProvider->getConfig($className, 'owner');
-                    $label  = $config->get('label');
-                }
-            }*/
 
             $organizationField = $environment->render(
                 self::ORGANIZATION_FIELD_TEMPLATE,
                 [
                     'form'  => $form,
-                    'label' => $label
+                    'label' => false
                 ]
             );
 
             /**
-             * Setting organization field as last field in first data block
+             * Setting organization field as first field in first data block
              */
             if (!empty($data['dataBlocks'])) {
                 if (isset($data['dataBlocks'][0]['subblocks'])) {
-                    $data['dataBlocks'][0]['subblocks'][0]['data'][] = $organizationField;
+                    array_unshift($data['dataBlocks'][0]['subblocks'][0]['data'], $organizationField);
                 }
             }
 
