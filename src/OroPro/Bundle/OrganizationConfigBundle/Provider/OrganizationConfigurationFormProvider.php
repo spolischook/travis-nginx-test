4f06612acfe3f85170c05ec1bad36bef927f92b2
yurio <yurio@ua.fm>
2015-01-22 13:54:56 +0200

    OEE-343, OEE-524: Organization configuration form provider

---
+++ src/OroPro/Bundle/OrganizationConfigBundle/Provider/OrganizationConfigurationFormProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OroPro\Bundle\OrganizationConfigBundle\Provider;
+
+use Oro\Bundle\ConfigBundle\Provider\SystemConfigurationFormProvider;
+
+class OrganizationConfigurationFormProvider extends SystemConfigurationFormProvider
+{
+    const ORGANIZATION_TREE_NAME  = 'organization_configuration';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTree()
+    {
+        return $this->getTreeData(self::ORGANIZATION_TREE_NAME, self::CORRECT_FIELDS_NESTING_LEVEL);
+    }
+}
