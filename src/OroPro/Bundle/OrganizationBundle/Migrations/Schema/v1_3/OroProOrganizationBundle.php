66512b8565580db4cded7ff8b64d230e8609bfbf
Makar Sichevoy <sichevoy@gmail.com>
2015-03-13 18:49:03 +0200

    CRM-2564: Add implementation user configuration scope for EE

--- src/OroPro/Bundle/OrganizationBundle/Migrations/Schema/v1_3/OroProOrganizationBundle.php
+++ src/OroPro/Bundle/OrganizationBundle/Migrations/Schema/v1_3/OroProOrganizationBundle.php
@@ -26,12 +26,12 @@ class OroProOrganizationBundle implements Migration
     {
         $table = $schema->createTable('oro_pro_organization_user');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
-        $table->addColumn('user_id', 'integer');
-        $table->addColumn('organization_id', 'integer');
+        $table->addColumn('user_id', 'integer', ['notnull' => false]);
+        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
         $table->setPrimaryKey(['id']);
 
-        $table->addIndex(['user_id'], 'IDX_AA37765432C8A3DE');
-        $table->addIndex(['organization_id'], 'IDX_AA377654A76ED395');
+        $table->addIndex(['user_id'], 'IDX_AA377654A76ED395');
+        $table->addIndex(['organization_id'], 'IDX_AA37765432C8A3DE');
         $table->addUniqueIndex(['user_id', 'organization_id'], 'oro_pro_organization_user');
 
         $table->addForeignKeyConstraint(
