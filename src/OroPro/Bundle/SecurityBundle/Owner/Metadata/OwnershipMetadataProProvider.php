7c6aee1fa7ba9bb8d7849fdb3896a0adea5812cf
Dmitry Naydenov <dnaidonov@magecore.com1~>
2015-03-25 18:27:23 +0200

    OEE-548 Update system organization ACL logic fix review issue

--- src/OroPro/Bundle/SecurityBundle/Owner/Metadata/OwnershipMetadataProProvider.php
+++ src/OroPro/Bundle/SecurityBundle/Owner/Metadata/OwnershipMetadataProProvider.php
@@ -39,6 +39,7 @@ class OwnershipMetadataProProvider extends OwnershipMetadataProvider
             $organizationFieldName,
             $organizationColumnName
         );
+
         return $data;
     }
 }
