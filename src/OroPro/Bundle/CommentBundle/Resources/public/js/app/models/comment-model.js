ce2546ed20f581545be8f8f9ce80fed952f9686d
Andrii Muzalevskyi <amuzalevsky@magecore.com>
2015-03-31 18:35:04 +0300

    CRM-2881 comments design update

--- src/OroPro/Bundle/CommentBundle/Resources/public/js/app/models/comment-model.js
+++ src/OroPro/Bundle/CommentBundle/Resources/public/js/app/models/comment-model.js
@@ -2,78 +2,27 @@
 define(function (require) {
     'use strict';
 
-    var CommentModel,
-        Chaplin = require('chaplin'),
-        routing = require('routing'),
-        BaseModel = require('oroui/js/app/models/base/model');
+    var ProCommentModel,
+        CommentModel = require('orocomment/js/app/models/comment-model');
 
-    CommentModel = BaseModel.extend({
-        route: 'oro_api_comment_get_item',
-        routeRemoveAttachment: 'oro_api_comment_remove_attachment_item',
-
-        defaults: {
-            owner: '',
-            owner_id: null,
-            editor: '',
-            editor_id: null,
-            message: '',
-            relationClass: null,
-            relationId: null,
-            createdAt: '',
-            updatedAt: '',
-            attachmentURL: null,
-            attachmentFileName: null,
-            attachmentSize: null,
-            editable: true,
-            removable: true
-        },
-
-        initialize: function() {
-            CommentModel.__super__.initialize.apply(this, arguments);
-            this.on('request', this.beginSync);
-            this.on('sync', this.finishSync);
-            this.on('error', this.unsync);
-        },
-
+    ProCommentModel = CommentModel.extend({
+        /**
+         * @inheritDoc
+         */
         url: function () {
-            var url, parameters;
-            if (!this.isNew()) {
-                parameters = {
-                    id: this.get('id'),
-                    _format: 'json'
-                };
-                url = routing.generate(this.route, parameters);
-            } else {
-                url = CommentModel.__super__.url.call(this);
-            }
+            var url, _sa_org_id;
 
-            var _sa_org_id = $('input#_sa_org_id').val();
+            url = ProCommentModel.__super__.url.call(this, arguments);
+
+            // Need to add organization id to url
+            _sa_org_id = $('input#_sa_org_id').val();
             if (_sa_org_id) {
                 url = url + (url.indexOf('?') == -1 ? '?' : '&') + '_sa_org_id=' + _sa_org_id;
             }
 
             return url;
-        },
-
-        removeAttachment: function() {
-            var model = this,
-                url = routing.generate(this.routeRemoveAttachment, {id: model.id});
-            return $.ajax({
-                url: url,
-                type: 'POST',
-                success: function () {
-                    model.set('attachmentURL', null);
-                    model.set('attachmentFileName', null);
-                    model.set('attachmentSize', null);
-                },
-                error: function (jqxhr) {
-                    model.trigger('error', model, jqxhr);
-                }
-            });
         }
     });
 
-    _.extend(CommentModel.prototype, Chaplin.SyncMachine);
-
-    return CommentModel;
+    return ProCommentModel;
 });
