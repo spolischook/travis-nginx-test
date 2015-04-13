5db89e5e3e28194c6bf279c5895b43456ac97fb4
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-04-13 13:59:21 +0300

    CRM-2777:Add calendar connections

--- Migrations/Data/B2C/ORM/LoadUsersCalendarData.php
+++ Migrations/Data/B2C/ORM/LoadUsersCalendarData.php
@@ -1,6 +1,8 @@
 <?php
 namespace OroCRMPro\Bundle\DemoDataBundle\Migrations\Data\B2C\ORM;
 
+use Doctrine\DBAL\Events;
+use Oro\Bundle\CalendarBundle\Entity\CalendarProperty;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Doctrine\ORM\EntityRepository;
@@ -63,6 +65,7 @@ class LoadUsersCalendarData extends AbstractFixture implements DependentFixtureI
     {
         return [
             'events' => $this->loadData('calendar/events.csv'),
+            'connections' => $this->loadData('calendar/connections.csv')
         ];
     }
 
@@ -91,6 +94,7 @@ class LoadUsersCalendarData extends AbstractFixture implements DependentFixtureI
             }
 
             $created = $calendar->getOwner()->getCreatedAt();
+            $this->em->getClassMetadata('Oro\Bundle\CalendarBundle\Entity\CalendarEvent')->setLifecycleCallbacks([]);
 
             for ($i = 0; array_key_exists($i, $data['events']); $created->add(new \DateInterval('P1D'))) {
                 $dayEvents = array_key_exists($i, $events) ? $events[$i]: [];
@@ -98,18 +102,45 @@ class LoadUsersCalendarData extends AbstractFixture implements DependentFixtureI
                     foreach ($dayEvents as $eventData) {
                         $event = new CalendarEvent();
                         $this->setObjectValues($event, $eventData);
+                        $start = new \DateTime($created->format('Y-m-d') . ' ' . $eventData['start time']);
+                        $end = new \DateTime($created->format('Y-m-d') . ' ' . $eventData['end time']);
+                        $createdAt = clone $start;
                         $event
-                            ->setStart(new \DateTime($created->format('Y-m-d') . ' ' . $eventData['start time']))
-                            ->setEnd(new \DateTime($created->format('Y-m-d') . ' ' . $eventData['end time']));
-
+                            ->setStart($start)
+                            ->setEnd($end)
+                            ->setCreatedAt($createdAt->modify('-1 day'));
+                        $event->setUpdatedAt(
+                            (new \DateTime())->setTimestamp(rand($start->getTimestamp(), $end->getTimestamp()))
+                        );
                         $calendar->addEvent($event);
                         $this->setSecurityContext($calendar->getOwner());
-                        $manager->persist($calendar);
                     }
                     $i++;
                 }
             }
         }
+
+        foreach ($data['connections'] as $connection) {
+            $user = $this->getUserReference($connection['user_uid']);
+            $targetUser = $this->getUserReference($connection['target_user_uid']);
+            $calendar = $this->calendarRepository->findDefaultCalendar(
+                $user->getId(),
+                $user->getOrganization()->getId()
+            );
+            $targetCalendar = $this->calendarRepository->findDefaultCalendar(
+                $targetUser->getId(),
+                $targetUser->getOrganization()->getId()
+            );
+            $calendarProperty = new CalendarProperty();
+            $calendarProperty
+                ->setTargetCalendar($targetCalendar)
+                ->setCalendarAlias('user')
+                ->setCalendar($calendar->getId())
+                ->setPosition($connection['position'])
+                ->setBackgroundColor($connection['background_color']);
+            $this->em->persist($calendarProperty);
+        }
+
         $manager->flush();
     }
 
