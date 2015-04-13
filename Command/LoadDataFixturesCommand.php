6e26962bd5465b1f7fe9abc343399f0cd04a01e8
Maksym Perepelytsya <mperepelytsya@magecore.com>
2015-04-13 14:54:49 +0300

    CRM-3121: Move oro:cron:analytic:calculate from jobs to base command.

--- Command/LoadDataFixturesCommand.php
+++ Command/LoadDataFixturesCommand.php
@@ -2,7 +2,9 @@
 
 namespace OroCRMPro\Bundle\DemoDataBundle\Command;
 
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 use Oro\Bundle\MigrationBundle\Command\LoadDataFixturesCommand as BaseDataFixturesCommand;
 
@@ -19,13 +21,28 @@ class LoadDataFixturesCommand extends BaseDataFixturesCommand
     }
 
     /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $parentReturnCode = parent::execute($input, $output);
+        if ($parentReturnCode === 0) {
+            $command = $this->getApplication()->find('oro:cron:analytic:calculate');
+            $input = new ArrayInput(['command' => 'oro:cron:analytic:calculate']);
+            $command->run($input, $output);
+        }
+        return $parentReturnCode;
+    }
+
+
+    /**
      * @param InputInterface $input
      * @return string
      */
     protected function getFixtureRelativePath(InputInterface $input)
     {
         $fixtureRelativePath = self::MAIN_FIXTURES_PATH;
-        if($this->getTypeOfFixtures($input)) {
+        if ($this->getTypeOfFixtures($input)) {
             $fixtureRelativePath = 'Migrations/Data/' . $this->getTypeOfFixtures($input) . '/ORM';
         }
 
