c43e02b95514e0be0d2b91f21563867c4c637f27
Denys Voronin <dvoronin@magecore.com>
2015-05-19 20:33:07 +0300

    OEE-558: Fix tests.

--- src/OroCRMPro/Bundle/OutlookBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
+++ src/OroCRMPro/Bundle/OutlookBundle/Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -3,6 +3,9 @@
 namespace OroCRMPro\Bundle\OutlookBundle\Tests\Unit\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Translation\MessageSelector;
+
+use Oro\Bundle\TranslationBundle\Translation\Translator;
 
 use OroCRMPro\Bundle\OutlookBundle\DependencyInjection\Configuration;
 
@@ -10,7 +13,10 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetConfigTreeBuilder()
     {
-        $config    = new Configuration();
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerBuilder');
+        $translator = new Translator($container, new MessageSelector());
+
+        $config    = new Configuration($translator);
         $processor = new Processor();
 
         $actualConfiguration = $processor->processConfiguration($config, []);
