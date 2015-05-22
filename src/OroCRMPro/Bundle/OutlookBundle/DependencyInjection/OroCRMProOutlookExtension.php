045d2579083bc4c31785e13853c09b2e50e92e71
Vova Soroka <vsoroka@magecore.com>
2015-05-22 15:21:55 +0300

    OEE-558: Add data transformer

--- src/OroCRMPro/Bundle/OutlookBundle/DependencyInjection/OroCRMProOutlookExtension.php
+++ src/OroCRMPro/Bundle/OutlookBundle/DependencyInjection/OroCRMProOutlookExtension.php
@@ -14,12 +14,11 @@ class OroCRMProOutlookExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-
         $configuration = new Configuration();
+        $config        = $this->processConfiguration($configuration, $configs);
 
-        $config = $this->processConfiguration($configuration, $configs);
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
 
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
     }
