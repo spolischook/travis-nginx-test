26dda0f1f702fba61937fdd0c34b79702d62606e
Vova Soroka <vsoroka@magecore.com>
2015-05-22 16:06:56 +0300

    Merge branch 'feature/OEE-556_API_to_work_with_emails' into ticket/OEE-561

--- src/OroCRMPro/Bundle/OutlookBundle/DependencyInjection/OroCRMProOutlookExtension.php
+++ src/OroCRMPro/Bundle/OutlookBundle/DependencyInjection/OroCRMProOutlookExtension.php
@@ -20,9 +20,6 @@ class OroCRMProOutlookExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
     }
 }
