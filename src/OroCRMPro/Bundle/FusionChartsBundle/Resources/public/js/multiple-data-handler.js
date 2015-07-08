c71dabe8c1033e8431ff1c999cba45b1f4c1263f
Hryhorii Hrebiniuk <hhrebiniuk@magecore.com>
2015-07-08 11:53:27 +0300

    BAP-8227: Autoformat JS - use external tool to format all JS code
     - jscs and jshint fixes

--- src/OroCRMPro/Bundle/FusionChartsBundle/Resources/public/js/multiple-data-handler.js
+++ src/OroCRMPro/Bundle/FusionChartsBundle/Resources/public/js/multiple-data-handler.js
@@ -11,15 +11,15 @@ define([
      * @name   multipleHandler
      */
     return function(dataSource, options, isCurrencyPrepend) {
-        if (options.data_schema.value.type == 'percent') {
-            dataSource.chart['numberSuffix'] = '%';
-        } else if (options.data_schema.value.type == 'currency') {
+        if (options.data_schema.value.type === 'percent') {
+            dataSource.chart.numberSuffix = '%';
+        } else if (options.data_schema.value.type === 'currency') {
             if (isCurrencyPrepend !== null) {
                 var currencySymbol = localeSettings.getCurrencySymbol();
                 var symbolPosition = 'number' + (isCurrencyPrepend ? 'Prefix' : 'Suffix');
                 dataSource.chart[symbolPosition] = currencySymbol;
             }
-            dataSource.chart['forceDecimals'] = '1';
+            dataSource.chart.forceDecimals = '1';
         }
 
         var max = 0;
@@ -29,7 +29,7 @@ define([
             var prev = 0;
             _.each(dataset.data, function(data) {
                 data.displayValue = dataFormatter.formatValue(data.value, options.data_schema.value.type);
-                if (options.data_schema.value.type == 'percent') {
+                if (options.data_schema.value.type === 'percent') {
                     data.value *= 100;
                 } else {
                     data.value = data.value === null ?
@@ -54,7 +54,7 @@ define([
         });
 
         _.each(dataSource.categories.category, function(category) {
-            if (category.label != null) {
+            if (category.label !== null && category.label !== void 0) {
                 var labelValue = dataFormatter.parseValue(category.label, options.data_schema.label.type);
                 category.label = labelValue === null ?
                     category.label : dataFormatter.formatValue(labelValue, options.data_schema.label.type);
@@ -68,6 +68,6 @@ define([
          */
         this.getDataSource = function() {
             return dataSource;
-        }
-    }
+        };
+    };
 });
