4e0dd8592619690de54b64ce348e3bb9c88f3dd4
Valentine Velikiy <vvelykyi@magecore.com>
2015-07-23 19:30:15 +0300

    AEIV-145: Implement Decision Maker

--- src/OroPro/Bundle/SecurityBundle/Owner/EntityOwnershipDecisionMaker.php
+++ src/OroPro/Bundle/SecurityBundle/Owner/EntityOwnershipDecisionMaker.php
@@ -2,6 +2,11 @@
 
 namespace OroPro\Bundle\SecurityBundle\Owner;
 
+use Doctrine\Common\Util\ClassUtils;
+
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
+
+use Oro\Bundle\SecurityBundle\Acl\Domain\BusinessUnitSecurityIdentity;
 use Oro\Bundle\SecurityBundle\Owner\EntityOwnershipDecisionMaker as BaseDecisionMaker;
 
 use OroPro\Bundle\SecurityBundle\Acl\Domain\OrganizationSecurityIdentity;
@@ -34,5 +39,31 @@ class EntityOwnershipDecisionMaker extends BaseDecisionMaker
         }
 
         return false;
+    }
+
+    /**
+     * @param object $domainObject
+     * @return bool
+     */
+    protected function isSharingApplicable($domainObject)
+    {
+        $entityName = ClassUtils::getClass($domainObject);
+        $shareScopes = $this->configProvider->hasConfig($entityName)
+            ? $this->configProvider->getConfig($entityName)->get('share_scopes')
+            : null;
+        if (!$this->ace || !$shareScopes) {
+            return false;
+        }
+
+        $sharedToScope = false;
+        if ($this->ace->getSecurityIdentity() instanceof UserSecurityIdentity) {
+            $sharedToScope = 'user';
+        } elseif ($this->ace->getSecurityIdentity() instanceof BusinessUnitSecurityIdentity) {
+            $sharedToScope = 'business_unit';
+        } elseif ($this->ace->getSecurityIdentity() instanceof OrganizationSecurityIdentity) {
+            $sharedToScope = 'organization';
+        }
+
+        return in_array($sharedToScope, $shareScopes);
     }
 }
