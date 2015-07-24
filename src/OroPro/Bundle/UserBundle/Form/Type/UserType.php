a4f8bdacc246a8f2d8cfb3c807fcd6f0f5b9c961
alex gotgelf <gotgelf@gmail.com>
2015-07-24 20:18:18 +0200

    AEIV-113: UI User's management modifications

--- src/OroPro/Bundle/UserBundle/Form/Type/UserType.php
+++ src/OroPro/Bundle/UserBundle/Form/Type/UserType.php
@@ -3,12 +3,43 @@
 namespace OroPro\Bundle\UserBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 use Oro\Bundle\UserBundle\Form\Type\UserType as BaseUserType;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
+use Oro\Bundle\ConfigBundle\Manager\UserConfigManager;
+use Doctrine\ORM\EntityManager;
 use OroPro\Bundle\UserBundle\EventListener\FormUserTypeSubscriber;
 
 class UserType extends BaseUserType
 {
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @param SecurityContextInterface $security
+     * @param SecurityFacade $securityFacade
+     * @param Request $request
+     * @param UserConfigManager $userConfigManager
+     * @param EntityManager $em
+     */
+    public function __construct(
+        SecurityContextInterface $security,
+        SecurityFacade           $securityFacade,
+        Request                  $request,
+        UserConfigManager        $userConfigManager,
+        EntityManager            $em
+    ) {
+        parent::__construct($security, $securityFacade, $request, $userConfigManager);
+        $this->em = $em;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -16,5 +47,41 @@ class UserType extends BaseUserType
     {
         parent::buildForm($builder, $options);
         $builder->addEventSubscriber(new FormUserTypeSubscriber($this->security, $this->isMyProfilePage));
+    }
+
+    /**
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['permissions_map'] = json_encode($this->getRolesToOrganizationsMap());
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRolesToOrganizationsMap()
+    {
+        $roles = $this->em->createQueryBuilder()
+            ->select('r')
+            ->from('Oro\Bundle\UserBundle\Entity\Role', 'r')
+            ->andWhere('r.role <> :anon')
+            ->setParameter('anon', User::ROLE_ANONYMOUS)
+            ->getQuery()->getResult();
+
+
+        $permissionsMap = [];
+        foreach ($roles as $role) {
+            $organization = $role->getOrganization();
+            $orgId = null;
+            if ($organization) {
+                $orgId = $organization->getId();
+            };
+            $permissionsMap[$role->getId()] = $orgId;
+        }
+
+        return $permissionsMap;
     }
 }
