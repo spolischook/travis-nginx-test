06dd1c982e607a3a8fb6d4e872860b75c00985af
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-07-27 18:14:30 +0200

    BAP-8633: Add logo for LDAP integration

--- Provider/ChannelType.php
+++ Provider/ChannelType.php
@@ -2,8 +2,9 @@
 namespace OroCRMPro\Bundle\LDAPBundle\Provider;
 
 use Oro\Bundle\IntegrationBundle\Provider\ChannelInterface;
+use Oro\Bundle\IntegrationBundle\Provider\IconAwareIntegrationInterface;
 
-class ChannelType implements ChannelInterface
+class ChannelType implements ChannelInterface, IconAwareIntegrationInterface
 {
     const TYPE = 'ldap';
 
@@ -13,5 +14,13 @@ class ChannelType implements ChannelInterface
     public function getLabel()
     {
         return 'orocrmpro.ldap.integration.channel.label';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIcon()
+    {
+        return 'bundles/orocrmproldap/img/ldap_logo.png';
     }
 }
