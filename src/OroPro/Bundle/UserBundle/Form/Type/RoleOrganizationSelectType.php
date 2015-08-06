a502d40eda8f5a2cd9cc651612edf78fbcb3579c
alex gotgelf <gotgelf@gmail.com>
2015-08-06 19:20:04 +0200

    AEIV-173: Validate implementation with business requirements

--- src/OroPro/Bundle/UserBundle/Form/Type/RoleOrganizationSelectType.php
+++ src/OroPro/Bundle/UserBundle/Form/Type/RoleOrganizationSelectType.php
@@ -2,11 +2,14 @@
 
 namespace OroPro\Bundle\UserBundle\Form\Type;
 
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
+use Oro\Bundle\UserBundle\Entity\User;
 use OroPro\Bundle\UserBundle\Helper\UserProHelper;
 
 class RoleOrganizationSelectType extends AbstractType
@@ -34,6 +37,17 @@ class RoleOrganizationSelectType extends AbstractType
     /**
      * {@inheritdoc}
      */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $user = $this->getUser();
+        if (!$this->userHelper->isUserAssignedToSystemOrganization($user)) {
+            $view->vars['required'] = true;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $defaults = [
@@ -43,9 +57,10 @@ class RoleOrganizationSelectType extends AbstractType
             'autocomplete_alias' => 'oropro_user_organizations',
         ];
 
-        $user = $this->securityContext->getToken()->getUser();
+        $user = $this->getUser();
         if (!$this->userHelper->isUserAssignedToSystemOrganization($user)) {
             $defaults['constraints'] = [new Assert\NotBlank()];
+            $defaults['attr']['data-validation'] = json_encode(['NotBlank' => []]);
         }
 
         $resolver->setDefaults(
@@ -67,5 +82,13 @@ class RoleOrganizationSelectType extends AbstractType
     public function getName()
     {
         return 'oropro_user_role_organization_select';
+    }
+
+    /**
+     * @return User
+     */
+    protected function getUser()
+    {
+        return $this->securityContext->getToken()->getUser();
     }
 }
