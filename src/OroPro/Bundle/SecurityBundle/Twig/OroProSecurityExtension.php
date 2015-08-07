17b57607e90c5aa748208cbe0f7938f2e4e4d749
Valentine Velikiy <vvelykyi@magecore.com>
2015-08-07 19:45:54 +0300

    AEIV-182: Modify shared status on entity view page

---
+++ src/OroPro/Bundle/SecurityBundle/Twig/OroProSecurityExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OroPro\Bundle\SecurityBundle\Twig;
+
+use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
+
+use Oro\Bundle\SecurityBundle\Twig\OroSecurityExtension as BaseExtension;
+
+use OroPro\Bundle\SecurityBundle\Acl\Domain\OrganizationSecurityIdentity;
+
+class OroProSecurityExtension extends BaseExtension
+{
+    /**
+     * @param SecurityIdentityInterface $sid
+     *
+     * @return string
+     */
+    protected function getFormattedName(SecurityIdentityInterface $sid)
+    {
+        $result = parent::getFormattedName($sid);
+
+        if (!$result && $sid instanceof OrganizationSecurityIdentity) {
+            $organization = $this->manager->getRepository('OroOrganizationBundle:Organization')->find($sid->getId());
+            if ($organization) {
+                return $organization->getName();
+            }
+        }
+
+        return '';
+    }
+}
