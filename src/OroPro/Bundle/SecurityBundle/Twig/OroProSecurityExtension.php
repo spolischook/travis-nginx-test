f6b6450b1adeda58da72b90c8005d39d8c2e9cc1
Valentine Velikiy <vvelykyi@magecore.com>
2015-08-10 17:17:30 +0300

    AEIV-182: Modify shared status on entity view page. - Added sorting to extract first record from cache.

--- src/OroPro/Bundle/SecurityBundle/Twig/OroProSecurityExtension.php
+++ src/OroPro/Bundle/SecurityBundle/Twig/OroProSecurityExtension.php
@@ -2,8 +2,11 @@
 
 namespace OroPro\Bundle\SecurityBundle\Twig;
 
+use Symfony\Component\Security\Acl\Domain\Entry;
+use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
 
+use Oro\Bundle\SecurityBundle\Acl\Domain\BusinessUnitSecurityIdentity;
 use Oro\Bundle\SecurityBundle\Twig\OroSecurityExtension as BaseExtension;
 
 use OroPro\Bundle\SecurityBundle\Acl\Domain\OrganizationSecurityIdentity;
@@ -24,6 +27,31 @@ class OroProSecurityExtension extends BaseExtension
             if ($organization) {
                 return $organization->getName();
             }
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function compareEntries(Entry $entryA, Entry $entryB)
+    {
+        $result = parent::compareEntries($entryA, $entryB);
+
+        $sidA = $entryA->getSecurityIdentity();
+        $sidB = $entryB->getSecurityIdentity();
+        if ($sidA instanceof OrganizationSecurityIdentity && $sidB instanceof OrganizationSecurityIdentity) {
+            $idA = (int) $sidA->getId();
+            $idB = (int) $sidB->getId();
+
+            return $idA < $idB ? -1 : 1;
+        } elseif ($sidA instanceof OrganizationSecurityIdentity && ($sidB instanceof UserSecurityIdentity || $sidB instanceof BusinessUnitSecurityIdentity)) {
+            return -1;
+        } elseif ($sidA instanceof UserSecurityIdentity && $sidB instanceof OrganizationSecurityIdentity) {
+            return 1;
+        } elseif ($sidA instanceof BusinessUnitSecurityIdentity && $sidB instanceof OrganizationSecurityIdentity) {
+            return 1;
         }
 
         return $result;
