53177bf9b7929e727ba042ec84b7c98ffd55d239
Hryhorii Hrebiniuk <hhrebinuk@magecore.com>
2015-08-13 12:53:22 +0300

    OEE-653: Fusion bar charts are flat on the Dashboard
     - fixed code style in FusingChart.extend.js

--- src/OroCRMPro/Bundle/FusionChartsBundle/Resources/public/lib/FusionCharts.extend.js
+++ src/OroCRMPro/Bundle/FusionChartsBundle/Resources/public/lib/FusionCharts.extend.js
@@ -1,9 +1,11 @@
-define(function (require) {
+define(function(require) {
+    'use strict';
+
     var _ = require('underscore');
-        FusionCharts = require('orocrmprofusioncharts/lib/FusionCharts');
+    var FusionCharts = require('orocrmprofusioncharts/lib/FusionCharts');
     require('orocrmprofusioncharts/lib/FusionCharts.HC');
 
-    FusionCharts(["private", "OroCrmPro", function () {
+    FusionCharts(['private', 'OroCrmPro', function() {
         var SmartLabelManager;
         if (!this.hcLib || !_.isFunction(this.hcLib.SmartLabelManager)) {
             return;
@@ -11,9 +13,9 @@ define(function (require) {
 
         this.hcLib.Raphael._url = '';
         SmartLabelManager = this.hcLib.SmartLabelManager;
-        SmartLabelManager.prototype.dispose = _.wrap(SmartLabelManager.prototype.dispose, function (dispose) {
-            var parent,
-                container = this.parentContainer;
+        SmartLabelManager.prototype.dispose = _.wrap(SmartLabelManager.prototype.dispose, function(dispose) {
+            var parent;
+            var container = this.parentContainer;
             dispose.apply(this, arguments);
             if (container && (parent = container.parentNode)) {
                 parent.removeChild(container);
