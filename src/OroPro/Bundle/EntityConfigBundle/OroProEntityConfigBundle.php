0a3d8b2e98aa3c5f9a0ceffa7edeed936eb57d1a
Pavel Kargapoltsev <pkargapoltsev@magecore.com>
2015-11-10 13:00:15 +0200

    OEE-757: Move records sharing functionality to the OroCRM Enterprise version - replace entity config info template - moved twig extension for share to OroPro

---
+++ src/OroPro/Bundle/EntityConfigBundle/OroProEntityConfigBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OroPro\Bundle\EntityConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroProEntityConfigBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'OroEntityConfigBundle';
+    }
+}
