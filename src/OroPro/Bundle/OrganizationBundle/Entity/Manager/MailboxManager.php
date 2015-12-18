8f768e9939e1006f8b0faaed7592d0f743826058
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-12-18 18:36:15 +0100

    CRM-3997: Make all mailboxes available under System organization

--- src/OroPro/Bundle/OrganizationBundle/Entity/Manager/MailboxManager.php
+++ src/OroPro/Bundle/OrganizationBundle/Entity/Manager/MailboxManager.php
@@ -4,19 +4,19 @@ namespace OroPro\Bundle\OrganizationBundle\Entity\Manager;
 
 use Oro\Bundle\EmailBundle\Entity\Manager\MailboxManager as BaseMailboxManager;
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\UserBundle\Entity\User;
 
 class MailboxManager extends BaseMailboxManager
 {
     /**
      * {@inheritdoc}
-     * @param Organization
      */
-    public function findAvailableMailboxes($user, Organization $organization)
+    public function findAvailableMailboxes($user, Organization $organization = null)
     {
         /*
          * If global organization is used, don't filter by organization.
          */
-        if ($organization->getIsGlobal()) {
+        if (!$organization || $organization->getIsGlobal()) {
             $organization = null;
         }
 
@@ -24,5 +24,20 @@ class MailboxManager extends BaseMailboxManager
             ->createAvailableMailboxesQuery($user, $organization);
 
         return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findAvailableOrigins(User $user, Organization $organization)
+    {
+        if (!$organization->getIsGlobal()) {
+            return parent::findAvailableOrigins($user, $organization);
+        }
+
+        return $this->registry->getRepository('OroEmailBundle:EmailOrigin')->findBy([
+            'owner' => $user,
+            'isActive' => true,
+        ]);
     }
 }
