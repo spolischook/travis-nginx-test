bb96697e106cb684cc7603032ffa780629187d41
Vova Soroka <vsoroka@magecore.com>
2016-03-21 18:42:36 +0200

    Convert repository update to two way sync

--- tool/src/Oro/Cli/Command/Repository/Sync.php
+++ tool/src/Oro/Cli/Command/Repository/Sync.php
@@ -2,11 +2,12 @@
 
 namespace Oro\Cli\Command\Repository;
 
-use Oro\Cli\Command\RootCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+
+use Oro\Cli\Command\RootCommand;
 
 class Sync extends RootCommand
 {
@@ -14,27 +15,28 @@ class Sync extends RootCommand
      * @var array list of supported upstream repositories mapped to local subtrees
      */
     protected $repositories = array(
-        'application/platform'          => 'git@github.com:laboro/platform-application.git',
-        'application/crm'               => 'git@github.com:laboro/crm-application.git',
-        'application/crm-enterprise'    => 'git@github.com:laboro/crm-enterprise-application.git',
+        'application/platform'       => 'git@github.com:laboro/platform-application.git',
+        'application/crm'            => 'git@github.com:laboro/crm-application.git',
+        'application/crm-enterprise' => 'git@github.com:laboro/crm-enterprise-application.git',
 //        'application/commerce'          => 'git@github.com:laboro/b2b-dev.git',
 
-        'package/platform'              => 'git@github.com:laboro/platform.git',
-        'package/crm'                   => 'git@github.com:laboro/crm.git',
-        'package/crm-enterprise'        => 'git@github.com:laboro/crm-enterprise.git',
-        'package/commerce'              => 'git@github.com:laboro/b2b.git',
-        'package/dotmailer'             => 'git@github.com:laboro/OroCRMDotmailerBundle.git',
-        'package/ldap'                  => 'git@github.com:laboro/OroCRMProLDAPBundle.git',
-        'package/mailchimp'             => 'git@github.com:laboro/OroCRMMailChimpBundle.git',
-        'package/magento-abandoned-cart'=> 'git@github.com:laboro/OroCRMAbandonedCartBundle.git',
-        'package/google-hangout'        => 'git@github.com:laboro/OroCRMHangoutsCallBundle.git',
-        'package/serialized-fields'     => 'git@github.com:laboro/OroEntitySerializedFieldsBundle.git',
-        'package/demo-data'             => 'git@github.com:laboro/OroCRMProDemoDataBundle.git',
-        'package/zendesk'               => 'git@github.com:laboro/OroCRMZendeskBundle.git',
-        'package/magento-contact-us'    => 'git@github.com:laboro/OroCRMMagentoContactUsBundle.git',
+        'package/platform'               => 'git@github.com:laboro/platform.git',
+        'package/crm'                    => 'git@github.com:laboro/crm.git',
+        'package/crm-enterprise'         => 'git@github.com:laboro/crm-enterprise.git',
+        'package/commerce'               => 'git@github.com:laboro/b2b.git',
+        'package/dotmailer'              => 'git@github.com:laboro/OroCRMDotmailerBundle.git',
+        'package/ldap'                   => 'git@github.com:laboro/OroCRMProLDAPBundle.git',
+        'package/mailchimp'              => 'git@github.com:laboro/OroCRMMailChimpBundle.git',
+        'package/magento-abandoned-cart' => 'git@github.com:laboro/OroCRMAbandonedCartBundle.git',
+        'package/google-hangout'         => 'git@github.com:laboro/OroCRMHangoutsCallBundle.git',
+        'package/serialized-fields'      => 'git@github.com:laboro/OroEntitySerializedFieldsBundle.git',
+        'package/demo-data'              => 'git@github.com:laboro/OroCRMProDemoDataBundle.git',
+        'package/zendesk'                => 'git@github.com:laboro/OroCRMZendeskBundle.git',
+        'package/magento-contact-us'     => 'git@github.com:laboro/OroCRMMagentoContactUsBundle.git',
 
-        'documentation'                 => 'git@github.com:orocrm/documentation.git',
+        'documentation' => 'git@github.com:orocrm/documentation.git',
     );
+
     /**
      * {@inheritdoc}
      */
@@ -49,13 +51,10 @@ class Sync extends RootCommand
 
     /**
      * {@inheritdoc}
-     *
-     * @param InputInterface $input
-     * @param OutputInterface $output
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $path  = $input->getArgument('path');
+        $path = $input->getArgument('path');
 
         if ($path) {
             if (isset($this->repositories[$path])) {
@@ -64,6 +63,7 @@ class Sync extends RootCommand
                 );
             } else {
                 $output->writeln("There are no repository registered for \"{$path}\" path.");
+
                 return;
             }
         } else {
@@ -73,30 +73,73 @@ class Sync extends RootCommand
         /* Changing directory as subtree commands must be executed from the repository root */
         $currentDir = getcwd();
         chdir($this->getRootDir());
+        try {
+            $this->doSync($output, $repositories);
+        } catch (\Exception $e) {
+            $output->writeln($e->getMessage());
+        }
+        /* Restore original directory */
+        chdir($currentDir);
+    }
 
+    /**
+     * @param OutputInterface $output
+     * @param array           $repositories
+     */
+    protected function doSync(OutputInterface $output, array $repositories)
+    {
         $remotes = [];
-        exec('git remote', $remotes);
+        $this->execCmd('git remote', true, $remotes);
 
         foreach ($repositories as $codePath => $repository) {
             $alias = $codePath . '_upstream';
             $output->writeln("Working on {$codePath} subtree from {$repository} repository.");
-            if (in_array($alias, $remotes)) {
+            if (in_array($alias, $remotes, true)) {
                 /* Fetch master from remote */
-                exec("git fetch {$alias} master");
+                $this->execCmd("git fetch {$alias} master");
             } else {
                 /* Add remote repository if it was not added yet */
-                exec("git remote add -f {$alias} {$repository}");
+                $this->execCmd("git remote add -f {$alias} {$repository}");
             }
 
             /* Add subtree prefix for remote master */
-            exec("git subtree add --prefix={$codePath} {$alias} master");
+            $this->execCmd("git subtree add --prefix={$codePath} {$alias} master", false);
 
             /* Pull all updates from remote master */
-            exec("git subtree pull --prefix={$codePath} {$alias} master");
-
-            /* Push all subtree changes to remote upstream repository */
-            exec("git subtree push --prefix={$codePath} {$alias} master");
+            if ($this->execCmd("git subtree pull --prefix={$codePath} {$alias} master")) {
+                /* Push all subtree changes to remote upstream repository */
+                $this->execCmd("git subtree push --prefix={$codePath} {$alias} master");
+            }
         }
-        chdir($currentDir);
+    }
+    /**
+     * Executes an external program.
+     *
+     * @param string $cmd            The command line
+     * @param bool   $throwException Whether to throw exception in case command failed
+     * @param array  $output         The command output
+     *
+     * @return bool The execution status. TRUE if no errors; otherwise, FALSE.
+     */
+    protected function execCmd($cmd, $throwException = true, array &$output = array())
+    {
+        $returnCode = 0;
+
+        exec($cmd, $output, $returnCode);
+
+        if ($throwException && $returnCode) {
+            throw new \RuntimeException(
+                sprintf(
+                    'The "%s" command failed. Return code: %s.\n'
+                    . 'Please fix the issue and run the "repository:sync" command again.\n'
+                    . 'The "git reset --hard origin/master" command can be used to rollback changes'
+                    . ' made by "repository:sync" command.',
+                    $cmd,
+                    $returnCode
+                )
+            );
+        }
+
+        return 0 == $returnCode;
     }
 }
