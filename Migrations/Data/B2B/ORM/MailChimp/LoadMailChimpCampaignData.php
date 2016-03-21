39c1b905c06b9e19791aa0199055de1de1ac9230
Denis Voronin <dvoronin@magecore.com>
2016-03-21 19:40:53 +0200

    OEE-816: Add few more campaigns to Email Campaigns in demo data

---
+++ Migrations/Data/B2B/ORM/MailChimp/LoadMailChimpCampaignData.php
@@ -0,0 +1,9 @@
+<?php
+namespace OroCRMPro\Bundle\DemoDataBundle\Migrations\Data\B2B\ORM\MailChimp;
+
+use OroCRMPro\Bundle\DemoDataBundle\Migrations\Data\B2C\ORM\MailChimp\LoadMailChimpCampaignData
+    as BaseLoadMailChimpCampaignData;
+
+class LoadMailChimpCampaignData extends BaseLoadMailChimpCampaignData
+{
+}
