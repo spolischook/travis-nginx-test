de6ccebd2d487d2dc3e293c1ffcee275887e19fb
Denis Voronin <dvoronin@magecore.com>
2016-03-22 20:40:46 +0200

    OEE-815: Add physical mailer as a Campaign to demo data  - Split B2B and B2C data

--- Migrations/Data/B2C/ORM/LoadCampaignData.php
+++ Migrations/Data/B2C/ORM/LoadCampaignData.php
@@ -18,7 +18,9 @@ class LoadCampaignData extends AbstractFixture implements OrderedFixtureInterfac
             parent::getExcludeProperties(),
             [
                 'user uid',
-                'organization uid'
+                'organization uid',
+                'start date',
+                'end date'
             ]
         );
     }
@@ -45,10 +47,15 @@ class LoadCampaignData extends AbstractFixture implements OrderedFixtureInterfac
             $campaign = new Campaign();
             $campaign->setOwner($user);
             $campaign->setOrganization($this->getOrganizationreference($campaignData['organization uid']));
-            $created = $this->generateCreatedDate();
-            $campaign->setEndDate($this->generateUpdatedDate($created));
-            $campaign->setStartDate($created->modify('-1 week'));
 
+            if(!empty($campaignData['start date']) && !empty($campaignData['end date'])){
+                $campaign->setStartDate(new \DateTime($campaignData['start date']));
+                $campaign->setEndDate(new \DateTime($campaignData['end date']));
+            } else {
+                $created = $this->generateCreatedDate();
+                $campaign->setEndDate($this->generateUpdatedDate($created));
+                $campaign->setStartDate($created->modify('-1 week'));
+            }
             $this->setObjectValues($campaign, $campaignData);
             $manager->persist($campaign);
             $this->setCampaignReference($campaignData['uid'], $campaign);
