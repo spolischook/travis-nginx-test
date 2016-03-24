061fbe361d0c2f04bcdb65226784fad944bbbd8b
Roman Grebenchuk <rgrebenchuk@magecore.com>
2016-03-24 20:17:00 +0200

    - travis run all tests for master branch - revert

--- .travis.sh
+++ .travis.sh
@@ -3,8 +3,6 @@ set -e
 step=$1
 case $step in
      before_install)
-           # run all tests for master branch
-           if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then exit; fi; 
            set +e; 
            echo "Before installing...";
            diff=$(git diff --name-only $TRAVIS_COMMIT_RANGE);
