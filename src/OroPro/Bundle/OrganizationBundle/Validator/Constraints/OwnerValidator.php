020c1026e3649ab4e5ad8148bb7b06eafc1da8d7
Yurii Muratov <yurio@ua.fm>
2016-04-06 15:11:17 +0300

    BAP-10004: Owner validator

--- src/OroPro/Bundle/OrganizationBundle/Validator/Constraints/OwnerValidator.php
+++ src/OroPro/Bundle/OrganizationBundle/Validator/Constraints/OwnerValidator.php
@@ -3,20 +3,9 @@
 namespace OroPro\Bundle\OrganizationBundle\Validator\Constraints;
 
 use Oro\Bundle\OrganizationBundle\Validator\Constraints\OwnerValidator as BaseValidator;
-use OroPro\Bundle\OrganizationBundle\Provider\SystemAccessModeOrganizationProvider;
 
 class OwnerValidator extends BaseValidator
 {
-    /** @var SystemAccessModeOrganizationProvider */
-    protected $organizationProvider;
-
-    /**
-     * @param SystemAccessModeOrganizationProvider $organizationProvider
-     */
-    public function setOrganizationProvider(SystemAccessModeOrganizationProvider $organizationProvider)
-    {
-        $this->organizationProvider = $organizationProvider;
-    }
 
     /**
      * {@inheritdoc}
