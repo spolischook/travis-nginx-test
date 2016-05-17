685e264ff620c4a0f798bfc6a69297d0ebf283eb
Mykhailo Sulyma <msulima@magecore.com>
2016-05-17 20:04:05 +0300

    OEE-874: Share functionality doesn't work  - updated component-helper.js

--- src/OroPro/Bundle/SecurityBundle/Resources/public/js/app/helper/component-helper.js
+++ src/OroPro/Bundle/SecurityBundle/Resources/public/js/app/helper/component-helper.js
@@ -1,44 +1,12 @@
-define(function(require) {
+define(function() {
     'use strict';
 
-    var _ = require('underscore');
-
-    var ComponentHelper = {
+    return {
         extractModelsFromGridCollection: function(datagrid) {
-            var selectionState = datagrid.getSelectionState();
-            var inSet = selectionState.inset;
-            var models = [];
-            if (inSet) {
-                _.each(datagrid.collection.models, function(model) {
-                    if (Object.keys(selectionState.selectedIds).length > 0) {
-                        _.each(selectionState.selectedIds, function(selectedId) {
-                            if (selectedId === model.id) {
-                                models.push(model);
-                            }
-                        });
-                    }
-                });
-            } else {
-                _.each(datagrid.collection.models, function(model) {
-                    if (Object.keys(selectionState.selectedIds).length > 0) {
-                        _.each(selectionState.selectedIds, function(selectedId) {
-                            var selectedModelMatched = false;
-                            if (selectedId === model.id) {
-                                selectedModelMatched = true;
-                            }
-                            if (!selectedModelMatched) {
-                                models.push(model);
-                            }
-                        });
-                    } else {
-                        models.push(model);
-                    }
-                });
-            }
-
-            return models;
+            var state = datagrid.getSelectionState();
+            return datagrid.collection.filter(function(model) {
+                return (state.selectedIds.indexOf(model.get('id')) !== -1) === state.inset;
+            });
         }
     };
-
-    return ComponentHelper;
 });
