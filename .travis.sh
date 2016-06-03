2c1edbf82b6b4d64f5f6a2e78b13e12428312b7b
Yevhen Shyshkin <yshyshkin@magecore.com>
2016-06-03 18:12:52 +0200

    BB-3320: Add possibility to test OroCommerce update at Travis environment  - testing

--- .travis.sh
+++ .travis.sh
@@ -97,20 +97,6 @@ case $step in
                git clone git@github.com:laboro/Builds.git builds
                ls -l
                exit 0
-
-               case $DB in
-                  echo  "Restore DB ${UPDATE_FROM}...";
-                  mysql)
-                         mysql -u root -D ${dbname} < Builds/DBDumps/${UPDATE_FROM}.mysql.sql;
-                         sed -i "s/database_driver"\:".*/database_driver"\:" pdo_mysql/g; s/database_name"\:".*/database_name"\:" ${dbname}/g; s/database_user"\:".*/database_user"\:" root/g; s/database_password"\:".*/database_password"\:" ~/g" app/config/parameters_test.yml;
-                  ;;
-                  postgresql)
-                         psql -U postgres -c "CREATE DATABASE ${dbname} WITH lc_collate = 'C' template = template0;";
-                         psql -U postgres -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' -d ${dbname};
-                         sed -i "s/database_driver"\:".*/database_driver"\:" pdo_pgsql/g; s/database_name"\:".*/database_name"\:" ${dbname}/g; s/database_user"\:".*/database_user"\:" postgres/g; s/database_password"\:".*/database_password"\:" ~/g" app/config/parameters_test.yml;
-                  ;;
-               esac
-
                rm -r builds
           fi
     ;;
