5189306599d5f2cc1d75c00c31a1f9266ab69354
Yevhen Shyshkin <yshyshkin@magecore.com>
2016-07-06 16:36:14 +0200

    BB-3678: Replace OroB2B with Oro namespace in OroCommerce application  - fixed namespaces in commerce-enterprise

---
+++ package/commerce-enterprise/src/Oro/Bundle/WebsiteProBundle/Tests/Unit/Form/Type/WebsiteTypeTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OroB2B\Bundle\WebsiteBundle\Tests\Unit\Form\Type;
+
+use Symfony\Component\Form\Test\FormIntegrationTestCase;
+
+use Oro\Bundle\WebsiteProBundle\Form\Type\WebsiteType;
+
+class WebsiteTypeTest extends FormIntegrationTestCase
+{
+    /** @var  WebsiteType $type */
+    protected $formType;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->formType = new WebsiteType();
+    }
+
+    /**
+     * @param bool $isValid
+     * @param mixed $defaultData
+     * @param array $submittedData
+     * @param mixed $expectedData
+     * @param array $options
+     * @dataProvider submitProvider
+     */
+    public function testSubmit($isValid, $defaultData, $submittedData, $expectedData, array $options = [])
+    {
+        $form = $this->factory->create($this->formType, $defaultData, $options);
+        $this->assertEquals($defaultData, $form->getData());
+        $form->submit($submittedData);
+        $this->assertEquals($isValid, $form->isValid());
+        $this->assertEquals($expectedData, $form->getData());
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals(WebsiteType::NAME, $this->formType->getName());
+    }
+
+    /**
+     * @return array
+     */
+    public function submitProvider()
+    {
+        return [
+            'website_valid' => [
+                'isValid'       => true,
+                'defaultData'   => ['name' => 'OroCRM'],
+                'submittedData' => [
+                    'name' => 'OroCommerce'
+                ],
+                'expectedData'  => ['name' => 'OroCommerce']
+            ],
+        ];
+    }
+}
