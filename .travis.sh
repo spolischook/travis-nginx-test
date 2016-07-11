46f59ecf85d391f52e412efdf27c9d7c6200c564
Sergey Zhuravel <szhuravel@magecore.com>
2016-07-11 15:05:52 +0300

    BAP-10849: Change Travis config to test maintanance branches properly - split soap tests

--- .travis.sh
+++ .travis.sh
@@ -128,6 +128,10 @@ case $step in
              sphinx-build -nW -b html -d _build/doctrees . _build/html; 
           fi
           if [ ! -z "$TESTSUITE" ]; then
+             TEST_RUNNER_OPTIONS='--exclude-group=soap'
+             if [ ! -z "$SOAP" ]; then
+                 TEST_RUNNER_OPTIONS='--stderr --group=soap'
+             fi
              composer install --optimize-autoloader --no-interaction;
              if [ ! -z "$DB" ]; then
                 if [ ! -z "$UPDATE_FROM" ]; then
@@ -171,7 +175,7 @@ case $step in
                         DIRECTORY="${APPLICATION}_$i"
                     fi
                     cd $DIRECTORY
-                    { $TRAVIS_BUILD_DIR/tool/vendor/bin/phpunit --verbose --stderr --testsuite=$TESTSUITE-$i-of-$PARALLEL_PROCESSES > ../../result.$i 2>&1 ; echo "$?" > "../../code.$i" ; } &
+                    { $TRAVIS_BUILD_DIR/tool/vendor/bin/phpunit --verbose ${TEST_RUNNER_OPTIONS} --testsuite=$TESTSUITE-$i-of-$PARALLEL_PROCESSES > ../../result.$i 2>&1 ; echo "$?" > "../../code.$i" ; } &
                     PIDS[$i]=$!
                     cd ../..
                 done
@@ -209,7 +213,7 @@ case $step in
                     fi
                 done
              else
-                 php $TRAVIS_BUILD_DIR/tool/vendor/bin/phpunit --stderr --testsuite ${TESTSUITE};
+                 php $TRAVIS_BUILD_DIR/tool/vendor/bin/phpunit --testsuite ${TESTSUITE} ${TEST_RUNNER_OPTIONS};
              fi
           fi
           if [ ! -z "$CS" ]; then
