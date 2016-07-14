0fbe3946145bbee389d9086a0c22c6432e4bdf7a
Dmitriy Krushelnitskiy <d.krushelnitskiy@gmail.com>
2016-07-14 13:27:40 +0300

    BAP-10987: Add organization component in organization structure

--- src/OroPro/Bundle/SecurityBundle/Search/AclHelper.php
+++ src/OroPro/Bundle/SecurityBundle/Search/AclHelper.php
@@ -2,12 +2,18 @@
 
 namespace OroPro\Bundle\SecurityBundle\Search;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
+use Oro\Bundle\SearchBundle\Query\Query;
 use Oro\Bundle\SecurityBundle\Search\AclHelper as BaseAclHelper;
 
 use OroPro\Bundle\OrganizationBundle\Provider\SystemAccessModeOrganizationProvider;
 
 class AclHelper extends BaseAclHelper
 {
+    /** @var  RequestStack */
+    protected $request;
+
     /** @var SystemAccessModeOrganizationProvider */
     protected $organizationProvider;
 
@@ -17,6 +23,45 @@ class AclHelper extends BaseAclHelper
     public function setOrganizationProvider(SystemAccessModeOrganizationProvider $organizationProvider)
     {
         $this->organizationProvider = $organizationProvider;
+    }
+
+    /**
+     * @param RequestStack $request
+     */
+    public function setRequest(RequestStack $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param Query $query
+     * @param $expr
+     */
+    protected function addOrganizationLimits(Query $query, $expr)
+    {
+        $organization = $this->securityFacade->getOrganization();
+        if ($organization && $organization->getIsGlobal()) {
+            if ($this->request->getCurrentRequest()->query->has('organizations')) {
+                $organizations = $this->request->getCurrentRequest()->get('organizations');
+                if (strlen($organizations) > 0) {
+                    $organizations = explode(',', $organizations);
+
+                    if (count($organizations) > 1
+                        || (count($organizations) === 1 && $this->getOrganizationId() !== current($organizations))
+                    ) {
+                        $query->getCriteria()->andWhere($expr->in('integer.organization', $organizations));
+
+                        return;
+                    }
+                } else {
+                    $query->getCriteria()->andWhere($expr->in('integer.organization', [0]));
+
+                    return;
+                }
+            }
+        }
+
+        parent::addOrganizationLimits($query, $expr);
     }
 
     /**
