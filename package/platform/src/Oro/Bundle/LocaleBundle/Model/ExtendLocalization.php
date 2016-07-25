a214b8879dfe52683169b998136ce73db00901c0
Yauheni Kasenka <beta.screech@gmail.com>
2016-07-25 16:11:32 +0300

    BB-3749: Use currently selected Localization at localized fallback values - created Fallback Datagrid extension and FallbackFormatter - created oro_fallback_value twig method - created @fallback_value Layout expression - modified FallbackGenerator and used in Category, MenuItem, Product, Localization - updated datagrids and used `fallback` property instead of sql relation - updated layouts and used @fallback_value - updated twigs and used oro_fallback_value - removed direct usage of FallbackTrait and moved it to Generator - updated LocalizationHelper to detect Current Localization - created CurrentLocalizationExtension and used to detect Current Localization on FrontendLocalizationBundle - renamed `oro_frontend_localization.user_localization_manager` to `oro_frontend_localization.manager.user_localization`

--- package/platform/src/Oro/Bundle/LocaleBundle/Model/ExtendLocalization.php
+++ package/platform/src/Oro/Bundle/LocaleBundle/Model/ExtendLocalization.php
@@ -1,23 +1,14 @@
 <?php
 
-namespace OroB2B\Bundle\MenuBundle\Model;
-
-use Oro\Bundle\AttachmentBundle\Entity\File;
-use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
-
-use OroB2B\Bundle\MenuBundle\Entity\MenuItem;
+namespace Oro\Bundle\LocaleBundle\Model;
 
 /**
- * @method File getImage()
- * @method MenuItem setImage(File $image)
  * @method LocalizedFallbackValue getTitle()
  * @method LocalizedFallbackValue getDefaultTitle()
  */
-class ExtendMenuItem
+class ExtendLocalization
 {
     /**
-     * Constructor
-     *
      * The real implementation of this method is auto generated.
      *
      * IMPORTANT: If the derived class has own constructor it must call parent constructor.
